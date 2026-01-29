d4507371a6aa92f8cc1111cd5d78451eacc2a944
ptmjyothish1 <65949125+ptmjyothish1@users.noreply.github.com>
2026-01-29 14:15:16 +0100

    Update Application.java

--- src/main/java/com/example/helloworld/Application.java
+++ src/main/java/com/example/helloworld/Application.java
@@ -1,18 +1,11 @@
+
 package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-@RestController
 public class Application {
-
-    @GetMapping("/")
-    public String hello() {
-        return "Hello, World!";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
